edff7ec1f933046fd053ea65ab685c63b2240f62
acm9711 <alejandro@weweb.com>
2022-11-05 12:27:45 +0100

    update "while" condition and "array" constant

--- Boomerang/Boomerangs.js
+++ Boomerang/Boomerangs.js
@@ -1,12 +1,12 @@
-const array = [1, 2, 3, 4, 3, 4, 5, 4];
+const array = [2, 1, 2, 3, 3, 4, 2, 4];
 
 let boomerang = [];
 let index = 0;
 
-while (index < array.length - 1) {
+while (index < array.length - 2) {
   if (array[index] === array[index + 2]) {
     boomerang.push(array.slice(index, index + 3));
-    index += 2;
+    index += 3;
   } else {
     index++;
   }
