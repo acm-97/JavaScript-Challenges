bd5e0bec8aa47d1b7395466613919c1517a9730c
alejandro <alejandro97.acm@gmail.com>
2022-12-14 10:12:12 -0500

    feat(adventjs-2021):  add "wrapGifts"

---
+++ midudev/adventjs-2021/13/index.js
@@ -0,0 +1,9 @@
+export default function wrapGifts(gifts) {
+  if (gifts.length === 0) return gifts;
+
+  const startEnd = '*'.repeat(gifts[0].length + 2)
+  const arr = gifts.map(emojis => `*${emojis}*`)
+  return [startEnd,  ...arr, startEnd]
+}
+
+console.log(wrapGifts(["🏈🎸", "🎮🧸"]));
