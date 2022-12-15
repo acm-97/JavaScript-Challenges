09b79f921184da19f4e415cf8f2dfac868601f30
acm-97 <alejandro97.acm@gmail.com>
2022-12-15 08:51:14 +0100

    feat(adventjs-2021): update "decodeNumber"

--- midudev/adventjs-2021/16/index.js
+++ midudev/adventjs-2021/16/index.js
@@ -1,6 +1,24 @@
+/**
+ * It takes a string of symbols and returns the number they represent.
+ * @param symbols - a string of symbols that represent a number
+ * @returns The sum of the values of the symbols.
+ */
 export default function decodeNumber(symbols) {
-  // ¡No olvides compartir tu solución en redes!
-   return false
- }
+  const decodeObj = {
+    ".": 1,
+    ",": 5,
+    ":": 10,
+    ";": 50,
+    "!": 100,
+  };
 
-console.log(decodeNumber());
+  symbols = symbols.split("");
+  return symbols.reduce((acc, cur, i, arr) => {
+    const value = decodeObj[cur] || NaN;
+    const nextValue = decodeObj[arr[i + 1]];
+    if (value < nextValue) return acc - value;
+    return acc + value;
+  }, 0);
+}
+
+// console.log(decodeNumber(".........!"));
