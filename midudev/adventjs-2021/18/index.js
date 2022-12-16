febb4d503388e2912543bdb89678478bc385b2dc
alejandro <alejandro97.acm@gmail.com>
2022-12-16 12:03:11 -0500

    feat(adventjs-2021): add "fixFiles"

---
+++ midudev/adventjs-2021/18/index.js
@@ -0,0 +1,13 @@
+export default function fixFiles(files) {
+  const fixedFiles = [];
+  let filesCount = {};
+
+  files.forEach((file) => {
+    const f = !fixedFiles.includes(file) ? file : `${file}(${filesCount[file]})`
+    filesCount[file] = (filesCount[file] || 0) + 1;;
+    fixedFiles.push(f);
+  });
+  return fixedFiles;
+}
+
+console.log(fixFiles(["photo", "postcard", "photo", "photo", "video"]));
