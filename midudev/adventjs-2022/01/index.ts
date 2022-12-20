f0a1a150a137e1cc5fd38f83e270bd2f9f140f83
acm-97 <alejandro97.acm@gmail.com>
2022-12-20 08:10:06 +0100

    refactor(adventjs-2022): add docs

--- midudev/adventjs-2022/01/index.ts
+++ midudev/adventjs-2022/01/index.ts
@@ -1,4 +1,5 @@
 export default function wrapping(gifts: string[]): string[] | [] {
+  /* Returning the gifts array with the wrapping function applied to each element. */
   return gifts.map((value) => {
     const wrapper = "*".repeat(value.length + 2);
     return `${wrapper}\n*${value}*\n${wrapper}`;
