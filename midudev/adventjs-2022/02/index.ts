358bf02a49bfbe03bcecfac59062b63eecc3d970
acm-97 <alejandro97.acm@gmail.com>
2022-12-20 09:54:58 +0100

    refactor(adventjs-2022): add docs

--- midudev/adventjs-2022/02/index.ts
+++ midudev/adventjs-2022/02/index.ts
@@ -1,4 +1,6 @@
 function countHours(year: number, holidays: string[]) {
+  /* Iterating through the holidays array and checking if the day is a weekday. If it is, it adds 2 to
+  the accumulator. If it is not, it adds 0 to the accumulator. */
   return holidays.reduce((acc, curr) => {
     let day = new Date(`${curr}/${year}`);
     return day.getDay() > 0 && day.getDay() < 6 ? acc + 2 : acc + 0;
@@ -6,6 +8,8 @@ function countHours(year: number, holidays: string[]) {
 }
 
 export default function countHoursOptimezed(year: number, holidays: string[]) {
+  /* Filtering out the holidays that are on a weekend and then multiplying the length of the array by 2
+  to get the number of hours. */
   return (
     holidays.filter(
       (holiday) => ![0, 6].includes(new Date(`${year}/${holiday}`).getDay())
